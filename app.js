45bda1d666c00302d8fdeec554fb447b79d9890b
iurijunio <iurijunio5@gmail.com>
2024-01-17 10:52:36 -0300

    updates and fixes

--- app.js
+++ app.js
@@ -1,35 +1,88 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
+const api = "https://videoead.com/nuclemig/api/v2/index.php?";
+const token = "7507.93248870969tokenavancada";
 const port = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.post('/registraAtendimento', (req, res) => {
-  // Obtenha os dados do corpo da solicitação
-  const { campo1, campo2, campoN, protocolo } = req.body;
+app.post('/registraAtendimento', async (req, res) => {
+  try {
+    const { telefoneUsuario, cpfUsuario, nomeUsuario, protocolo } = req.body;
+    const { formData, config } = getAPIParamsConfig(telefoneUsuario, cpfUsuario, nomeUsuario, protocolo);
 
-  // Lógica para processar os dados e gerar a resposta
-  const codigoRetorno = "123";
-  const descricaoRetorno = "Código Inválido";
-  const valor = "";
-  const textoTicket = ""; // Mensagem com o token
-  const chaveCliente = "cliente123"; // CPF do cliente, protocolo e data //
+    try {
+      const escolaAvancadaAPI = await axios.post(`${api}usuarios/novo`, formData, config);
+      const { erro, resultado } = escolaAvancadaAPI.data;
 
-  // Construa o objeto de resposta
-  const resposta = {
-    codigo_retorno: codigoRetorno,
-    descricao_retorno: descricaoRetorno,
-    valor: valor,
-    texto_ticket: textoTicket,
-    chave_cliente: chaveCliente
-  }; 
+      if (resultado && resultado.login) {
+        res.status(200).json({
+          codigo_retorno: "00",
+          descricao_retorno: "Operação realizada com sucesso.",
+          valor: "10000",
+          texto_ticket: `Usuário: ${resultado.nome}+Login: ${resultado.login}+Senha: ${resultado.senha}`,
+          chave_cliente: `${cpfUsuario}${protocolo}${new Date().getTime()}`,
+        });
+      }
+      else {
+        res.status(400).json({
+          codigo_retorno: "-1",
+          descricao_retorno: `${resultado ? resultado : erro ? erro : "Código Inválido."}`,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        codigo_retorno: "-2",
+        descricao_retorno: "Falha no processamento.",
+      });
+    }
 
-
-  // Envie a resposta
-  res.json(resposta);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
 });
+
+
+function getAPIParamsConfig(telefoneUsuario, cpfUsuario, nomeUsuario, protocolo) {
+  // Dados do formulário
+  const formData = new URLSearchParams();
+  formData.append("token", token);
+  formData.append("fone", telefoneUsuario);
+  formData.append("cpf", cpfUsuario);
+  formData.append("nome", nomeUsuario);
+  formData.append("obs", protocolo);
+
+  // Configurações da solicitação
+  const config = {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  };
+
+  return { formData, config };
+}
+
+app.get('/listarCursos', async (req, res) => {
+
+  // Dados do formulário
+  const formData = new URLSearchParams();
+  formData.append('token', token);
+
+  // Configurações da solicitação
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  // Fazendo a solicitação POST
+  const escolaAvancadaAPI = await axios.post(`${api}cursos/listar`, formData, config);
+
+  const { erro, resultado } = escolaAvancadaAPI.data;
+  res.json(resultado);
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
